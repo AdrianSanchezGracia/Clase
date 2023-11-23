3e7cfc60dd153193eca6d81da22d6faaaa41dd07
AdrianSanchezGracia <151823336+AdrianSanchezGracia@users.noreply.github.com>
2023-11-23 09:29:53 +0100

    Add files via upload

---
+++ Argumentos/src/main/java/com/mycompany/argumentos/Argumentos1.java
@@ -0,0 +1,19 @@
+
+package com.mycompany.argumentos;
+
+public class Argumentos {
+
+    public static void main(String[] args) {
+    
+        String argumento1;
+        String argumento2;
+        
+        argumento1 = args[0];
+        argumento2 = args[1];
+        
+        System.out.println("Argumento1: " + argumento1);
+        System.out.println("Argumento2: " + argumento2);
+        
+        
+    }
+}
